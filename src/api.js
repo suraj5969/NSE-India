db8aaafc11aea4abe5dd839ef24cc3925f0e8ac4
suraj5969 <suraj5969@outlook.com>
2022-04-14 14:15:04 +0530

    fetch api data

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const serverless = require('serverless-http');
 
+const axios = require('axios')
+
 const app = express();
 
 const router = express.Router();
@@ -8,8 +10,44 @@ const router = express.Router();
 router.get('/', (req, res) => {
 
     res.json({
-        hello : 'hii'
+        hello: 'hii'
     })
+})
+
+router.get('/nse_data', async (req, res) => {
+
+    try {
+
+        const data = await axios.get('https://www.nseindia.com/api/option-chain-indices?symbol=NIFTY');
+        if (data.status === 200) {
+            res.json(data.data);
+        }
+        else {
+            res.send('data not received from nse')
+        }
+    }
+    catch (err) {
+        console.log(err);
+        res.send('got error in catch block for nse') 
+    }
+})
+
+router.get('/placeholder_data', async (req, res) => {
+
+    try {
+
+        const data = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        if (data.status === 200) {
+            res.json(data.data);
+        }
+        else {
+            res.send('data not received from jsonplaceholder')
+        }
+    }
+    catch (err) {
+        console.log(err);
+        res.send('got error in catch block for jsonplaceholder') 
+    }
 })
 
 app.use('/.netlify/functions/api', router); // path must route to lambda
